ba9006bef7b93d41a2202b70e8c04fd93bc84165
S1547494 <sanket.dharmadhikari@saint-gobain.com>
2019-07-22 16:58:32 +0530

    End of section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
 
 @NgModule({
    declarations: [
@@ -21,9 +23,13 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      BsDropdownModule.forRoot(),
       AppRoutingModule
    ],
-   providers: [ ErrorInterceptorProvider ],
+   providers: [ 
+      ErrorInterceptorProvider,
+      AlertifyService 
+   ],
    bootstrap: [
       AppComponent
    ]
