22efb784e0de20f14cb1ea664fafe40f3ae22520
Sanket Dharmadhikari <MY_NAME@example.com>
2019-09-29 11:47:34 +0530

    End of Section 10

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -35,5 +35,22 @@ namespace DatingApp.API.Controllers {
 
             return Ok (userToReturn);
         }
+
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorised();
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if(await _repo.SaveAll())
+                return NoContent();
+
+            throe new Exception($"Updating user {id} failed on save.");
+
+        }
     }
 }
